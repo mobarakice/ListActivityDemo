40873fa04bf3e5892cd7626a34067879dde37b7c
Mobdrak Hosen <mobarak.ice.06@gmail.com>
2017-02-17 17:42:35 +0600

    Update connect.php

--- Bloodbank_backend/connect.php
+++ Bloodbank_backend/connect.php
@@ -1,8 +1,8 @@
 <?php 
 $server_name = "localhost";
-$user_name = "atomap_db_user";
-$pass = "AtomAP432!";
-$db_name = "bloodbank";
+$user_name = "entry-user-name";
+$pass = "entry-user-pass";
+$db_name = "entry-db-name";
 //$con = new mysqli($server_name,$user_name,$pass,$db_name) or die("Connection fail");
 $con = mysqli_connect($server_name,$user_name,$pass,$db_name) or die("Connection failed");
 //echo"Connection successfull";
